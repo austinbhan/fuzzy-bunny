c24200480500218df3ca097486bd7ce1113e36b1
austinbhan <austinbhan@outlook.com>
2022-05-23 17:58:51 -0600

    window Event Listener not functioning/undefined

--- create/create.js
+++ create/create.js
@@ -12,7 +12,7 @@ form.addEventListener('submit', async (e) => {
     // prevent default
     e.preventDefault();
     // get the name and family id from the form
-    const bunnyForm = new FormData('form');
+    const bunnyForm = new FormData(form);
     console.log(bunnyForm.get('bunny-name'));
     // use createBunny to create a bunny with this name and family id
     await createBunny({ name: bunnyForm.get('bunny-name'), family_id: bunnyForm.get('family-id') });
@@ -22,15 +22,14 @@ form.addEventListener('submit', async (e) => {
 window.addEventListener('load', async () => {
     // let's dynamically fill in the families dropdown from supabase
     // grab the select HTML element from the DOM
-    const familySelect = document.getElementById('family-id');
+    const familySelect = await getFamilies();
     // go get the families from supabase
-    await getFamilies();
     // for each family
     for (let family of familySelect) {
     // create an option tag
         const option = document.createElement('option');
     // set the option's value and text content
-        option.value = family.id;
+        option.value = family.family_id;
         option.textContent = family.name;
     // and append the option to the select
         familySelect.append(option);
--- render-utils.js
+++
@@ -1,6 +0,0 @@
-export function renderOption(family) {
-    const option = document.createElement('option');
-    option.value = family.id;
-    option.textContent = family.name;
-    return option;
-}
