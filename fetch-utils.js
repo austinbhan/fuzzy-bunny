94c9f651c59f6e350fee0959fe667daaa102a5db
austinbhan <austinbhan@outlook.com>
2022-05-23 16:18:52 -0600

    Working createBunny(bunny) function in fetch-utils.js

--- fetch-utils.js
+++ fetch-utils.js
@@ -11,11 +11,7 @@ export function getUser() {
 export async function getFamilies() {
     // fetch all families and their bunnies
     const response = await client.from('loving_families').select('*, bunnies(*)');
-    if (response.error) {
-        console.log(response.error.message);
-    } else {
-        return response.data;
-    }
+    return checkError(response);
 }
 
 export async function deleteBunny(id) {
@@ -26,7 +22,7 @@ export async function deleteBunny(id) {
 
 export async function createBunny(bunny) {
     // create a bunny using the bunny argument
-
+    const response = await client.from('bunnies').insert(bunny);
     return checkError(response);
 }
 
